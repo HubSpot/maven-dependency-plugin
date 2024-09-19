92de5aa1673b45a085d4b42d4a60d4f5ad1dbbba
Gelila Sahle <gsahle@hubspot.com>
2024-09-19 14:03:51 -0400

    change variable name

--- src/main/java/org/apache/maven/plugins/dependency/analyze/AbstractAnalyzeMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/AbstractAnalyzeMojo.java
@@ -559,13 +559,13 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
                 writer.startElement("artifactId");
                 writer.writeText(artifact.getArtifactId());
                 writer.endElement();
-                String dependencyKey = String.format(
+                String key = String.format(
                         "%s:%s:%s:%s",
                         artifact.getGroupId(),
                         artifact.getArtifactId(),
                         artifact.getType(),
                         (artifact.getClassifier() != null ? ":" : artifact.getClassifier()));
-                if (!managedDependencies.contains(dependencyKey)) {
+                if (!managedDependencies.contains(key)) {
                     writer.startElement("version");
                     writer.writeText(artifact.getBaseVersion());
                     writer.endElement();
