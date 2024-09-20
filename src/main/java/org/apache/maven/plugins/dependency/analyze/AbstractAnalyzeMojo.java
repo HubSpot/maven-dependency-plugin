3c63c0ec82fc32b394f35973992d02deb57a07a2
Gelila Sahle <gsahle@hubspot.com>
2024-09-20 12:59:00 -0400

    fix the conditional

--- src/main/java/org/apache/maven/plugins/dependency/analyze/AbstractAnalyzeMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/AbstractAnalyzeMojo.java
@@ -560,11 +560,11 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
                 writer.writeText(artifact.getArtifactId());
                 writer.endElement();
                 String key = String.format(
-                        "%s:%s:%s:%s",
+                        "%s:%s:%s%s",
                         artifact.getGroupId(),
                         artifact.getArtifactId(),
                         artifact.getType(),
-                        (artifact.getClassifier() != null ? ":" : artifact.getClassifier()));
+                        (artifact.getClassifier() != null ? ":" + artifact.getClassifier() : ""));
                 if (!managedDependencies.contains(key)) {
                     writer.startElement("version");
                     writer.writeText(artifact.getBaseVersion());
