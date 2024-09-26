657aa0b6f61aff02dfd98223cdbf310326ceb47d
Jared Stehler <jstehler@hubspot.com>
2024-09-26 07:48:13 -0400

    add test for redundant version check

--- src/test/java/org/apache/maven/plugins/dependency/analyze/TestAnalyzeDuplicateMojo.java
+++ src/test/java/org/apache/maven/plugins/dependency/analyze/TestAnalyzeDuplicateMojo.java
@@ -26,6 +26,7 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.dependency.AbstractDependencyMojoTestCase;
 import org.apache.maven.plugins.dependency.testUtils.stubs.DuplicateDependencies2ProjectStub;
+import org.apache.maven.plugins.dependency.testUtils.stubs.DuplicateDependencies3ProjectStub;
 import org.apache.maven.plugins.dependency.testUtils.stubs.DuplicateDependenciesProjectStub;
 import org.apache.maven.project.MavenProject;
 
@@ -72,60 +73,101 @@ public class TestAnalyzeDuplicateMojo extends AbstractDependencyMojoTestCase {
         assertTrue(log.getContent().contains("junit:junit:jar"));
     }
 
+    public void testDuplicate3() throws Exception {
+        MavenProject project = new DuplicateDependencies3ProjectStub();
+        getContainer().addComponent(project, MavenProject.class.getName());
+
+        MavenSession session = newMavenSession(project);
+        getContainer().addComponent(session, MavenSession.class.getName());
+
+        File testPom = new File(getBasedir(), "target/test-classes/unit/duplicate-dependencies/plugin-config3.xml");
+        AnalyzeDuplicateMojo mojo = (AnalyzeDuplicateMojo) lookupMojo("analyze-duplicate", testPom);
+        assertNotNull(mojo);
+        DuplicateLog log = new DuplicateLog();
+        mojo.setLog(log);
+        mojo.execute();
+
+        assertTrue(log.getContent()
+                .contains("List of redundant dependency versions defined in <dependencies/> in " + "your pom.xml"));
+        assertTrue(log.getContent().contains("junit:junit:jar:3.8.1"));
+    }
+
     static class DuplicateLog implements Log {
         StringBuilder sb = new StringBuilder();
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void debug(CharSequence content) {
             print("debug", content);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void debug(CharSequence content, Throwable error) {
             print("debug", content, error);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void debug(Throwable error) {
             print("debug", error);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void info(CharSequence content) {
             print("info", content);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void info(CharSequence content, Throwable error) {
             print("info", content, error);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void info(Throwable error) {
             print("info", error);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void warn(CharSequence content) {
             print("warn", content);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void warn(CharSequence content, Throwable error) {
             print("warn", content, error);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void warn(Throwable error) {
             print("warn", error);
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void error(CharSequence content) {
             System.err.println("[error] " + content.toString());
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         */
         public void error(CharSequence content, Throwable error) {
             StringWriter sWriter = new StringWriter();
             PrintWriter pWriter = new PrintWriter(sWriter);
